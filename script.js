a8cacc9126a8c69a2b9a268b0148ba9cc0404bbe
Sandesh13.0_jrdev <107756643+Sandesh13fr@users.noreply.github.com>
2024-12-21 21:39:26 +0530

    cngs

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var close = document.querySelector("#full i")
+var close = document.querySelector("#hamburger i")
     var menu = document.querySelector(".language-opts i")
 
     var tl = gsap.timeline()
-    tl.to("#full", {
+    tl.to("#hamburger", {
         transform: 'translateX(0%)',
         duration: 1,
         opacity: 1
     })
-    tl.from("#full h1", {
+    tl.from("#hamburger h1", {
         x: 30,
         duration: 0.3,
         opacity: 0,
         stagger: 0.2
     })
 
-    tl.from("#full i", {
+    tl.from("#hamburger i", {
         scale: 0,
         opacity: 0,
         duration: 0.3
