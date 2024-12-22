958fd8fa5770a5e17f2e1b9ac7bfa8c1d4b2ab63
webdev-raj <codewithsapnap@gmail.com>
2024-12-22 17:52:09 +0530

    done

--- script.js
+++ script.js
@@ -91,30 +91,52 @@ document.addEventListener("DOMContentLoaded", () => {
     // Define translations
     const translations = {
         en: {
-            aboutUs: "About Us",
+            aboutUs: "My Company",
             acquisitionOptions: "Acquisition Options",
             space360: "360 space",
-            searchPlaceholder: "Search...",
             homeTitle: `Your Dream Car Awaits You`,
             homeSubtitle: "Acting with integrity, a driver of trust",
             homeText: "At Int'l Motor, we offer top-quality vehicles with precision and care. Whether you're buying or selling, our expert team ensures every car meets the highest standards for performance and appearance.",
-            buttonText: "Book Free Consultation",
+            buttonText: "Discover our brokerage solutions",
+            buttonText2: "Find vehical of your dreams",
             testimonialsTitle: "What our customers are saying about us",
-            aboutHeading:"Integrity and Excellence in all our work.",
-            aboutHeadingText: "Every endeavor at Int'l Motor has been a team-driven journey, where each individual has contributed to our success. Together, we've not only facilitated vehicle acquisitions but also built lasting relationships that define the essence of our business. "
+            // aboutHeading: "Integrity and Excellence in all our work.",
+            // aboutHeadingText: "Every endeavor at Int'l Motor has been a team-driven journey, where each individual has contributed to our success. Together, we've not only facilitated vehicle acquisitions but also built lasting relationships that define the essence of our business. ",
+            whatWeEngaged: "What We Engaged",
+            innovativeSolution: "Innovative solution designed to revolutionize the way of acquiring a vehicle",
+            dealWithClients: "Deal with Clients",
+            happyCustomers: "Happy Customers",
+            dealsLocked: "Deals Locked",
+            ecoResponsibility: "ECO-RESPONSIBILITY",
+            ecoResponsibilityText: "Int'l Motor is an international brokerage company that provides unique vehicle acquisition solutions for personal or professional use.",
+            customerRequirements: "CUSTOMER REQUIREMENTS",
+            customerRequirementsText: "Our company firmly establishes its narrative through unparalleled brokerage solutions and exclusive client projects, driven by a commitment to Integrity and Excellence.",
+            socialJustice: "SOCIAL JUSTICE",
+            socialJusticeText: "With its extensive resources, the company is committed to delivering the best acquisition services in the market. Whether you need new or used vehicles, construction or agricultural machinery, utility vehicles, motorcycles, special vehicles, or services for private individuals, Int'l Motor is dedicated to meeting your specific needs.",
+            readMore: "Read More"
         },
         fr: {
-            aboutUs: "À propos de nous",
-            acquisitionOptions: "Options d'acquisition",
+            aboutUs: "mon entreprise",
+            acquisitionOptions: "Options d'Acquisition",
             space360: "Espace 360",
-            searchPlaceholder: "Chercher...",
             homeTitle: "Votre voiture de rêve vous attend",
             homeSubtitle: "Agir avec intégrité, moteur de confiance",
             homeText: "Chez Int'l Motor, nous proposons des véhicules de qualité supérieure avec précision et soin. Que vous achetiez ou vendiez, notre équipe d'experts veille à ce que chaque voiture respecte les normes les plus élevées en matière de performances et d'apparence.",
-            buttonText: "Réservez une consultation gratuite",
+            buttonText: "Découvrez notre courtage solutions",
+            buttonText2: "Trouvez le véhicule de vos rêves",
             testimonialsTitle: "Ce que nos clients disent de nous",
-            aboutHeading:"Intégrité et Excellence dans tout notre travail.",
-            aboutHeadingText: "Chaque effort chez Int'l Motor a été une aventure collective, où chaque individu a contribué à notre réussite. Ensemble, nous avons non seulement facilité l'acquisition de véhicules, mais également construit des relations durables qui définissent l'essence de notre entreprise."
+            whatWeEngaged: "Ce que nous faisons",
+            innovativeSolution: "Solution innovante conçue pour révolutionner la façon d'acquérir un véhicule",
+            dealWithClients: "Traiter avec des clients",
+            happyCustomers: "Clients Satisfaits",
+            dealsLocked: "Accords Conclus",
+            ecoResponsibility: "ÉCO-RESPONSABILITÉ",
+            ecoResponsibilityText: "Int'l Motor est une société de courtage internationale qui fournit des solutions uniques d'acquisition de véhicules à usage personnel ou professionnel.",
+            customerRequirements: "EXIGENCES DES CLIENTS",
+            customerRequirementsText: "Notre entreprise établit fermement son récit à travers des solutions de courtage inégalées et des projets exclusifs pour les clients, animée par un engagement envers l'intégrité et l'excellence.",
+            socialJustice: "JUSTICE SOCIALE",
+            socialJusticeText: "Grâce à ses ressources considérables, l'entreprise s'engage à fournir les meilleurs services d'acquisition sur le marché. Que vous ayez besoin de véhicules neufs ou d'occasion, de machines de construction ou agricoles, de véhicules utilitaires, de motos, de véhicules spéciaux ou de services pour les particuliers, Int'l Motor est dédié à répondre à vos besoins spécifiques.",
+            readMore: "En savoir plus"
         }
     };
 
@@ -126,11 +148,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 el.textContent = translations[lang][key];
             }
         });
-        // Update placeholder for search input
-        const searchInput = document.getElementById("searchInput");
-        if (searchInput) {
-            searchInput.placeholder = translations[lang].searchPlaceholder;
-        }
     };
 
     // Event listeners for buttons
