4985046d5adc5eacde794c979135714d9922ad95
webdev-raj <codewithsapnap@gmail.com>
2024-12-22 15:42:25 +0530

    mini changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-purple': 'rgba(75, 11, 108, 0.15)',
+      },
+      boxShadow: {
+        'custom': '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
+      },
+      borderRadius: {
+        'custom': '10px',
+      },
+      backdropBlur: {
+        'custom': '11px',
+      },
+      borderColor: {
+        'custom-white': 'rgba(255, 255, 255, 0.18)',
+      },
+    },
   },
   plugins: [],
 }
